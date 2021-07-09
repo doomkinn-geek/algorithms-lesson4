49fcb71c3ddf198452faf13cef69128b98f3ae01
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-07-09 18:17:12 +0300

    GB tree

---
+++ task1/GBTree.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task2
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var node = obj as TreeNode;
+
+            if (node == null)
+                return false;
+
+            return node.Value == Value;
+        }
+        public TreeNode(int value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+        public TreeNode()
+        {
+            Value = default(int);
+            LeftChild = null;
+            RightChild = null;
+        }
+
+        public TreeNode(int value, TreeNode lChild, TreeNode rChild)
+        {
+            Value = value;
+            LeftChild = lChild;
+            RightChild = rChild;
+        }
+    }
+
+    public interface ITree
+    {
+        TreeNode GetRoot();
+        void AddItem(int value); // добавить узел
+        void RemoveItem(int value); // удалить узел по значению
+        TreeNode GetNodeByValue(int value); //получить узел дерева по значению
+        void PrintTree(); //вывести дерево в консоль
+    }
+
+    public class Tree : ITree
+    {
+        private TreeNode root;
+        public TreeNode Root
+        {
+            get { return root; }
+        }
+
+        public Tree()
+        {
+            root = null;
+        }
+
+        public Tree(int value)
+        {
+            TreeNode p = new TreeNode(value);
+            root = p;
+        }
+        public void AddItem(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TreeNode GetNodeByValue(int value)
+        {
+            TreeNode iterator = root;
+
+            while (iterator != null)
+            {
+                int compare = value - iterator.Value;
+                if (value == iterator.Value)
+                    return iterator;
+
+                if (compare < 0)
+                {
+                    iterator = iterator.LeftChild;
+                    continue;
+                }
+                iterator = iterator.RightChild;
+            }
+            return null;
+        }
+
+        public TreeNode GetRoot()
+        {
+            return root;
+        }
+
+        public void PrintTree()
+        {
+            NodeInfo[] nodes = TreeHelper.GetTreeInLine(this);
+            int level = 0;
+            for(int i = 0; i < nodes.Length; i++)
+            {                
+                if (level != nodes[i].Depth)
+                {
+                    Console.WriteLine();
+                    Console.Write("[{0}]", nodes[i].Node.Value);                    
+                }
+                else
+                {
+                    Console.Write("[{0}]", nodes[i].Node.Value);
+                }
+                level = nodes[i].Depth;
+            }
+        }
+
+        public void RemoveItem(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class TreeHelper
+    {
+        public static NodeInfo[] GetTreeInLine(ITree tree)
+        {
+            var bufer = new Queue<NodeInfo>();
+            var returnArray = new List<NodeInfo>();
+            var root = new NodeInfo() { Node = tree.GetRoot() };
+            bufer.Enqueue(root);
+
+            while (bufer.Count != 0)
+            {
+                var element = bufer.Dequeue();
+                returnArray.Add(element);
+
+                var depth = element.Depth + 1;
+
+                if (element.Node.LeftChild != null)
+                {
+                    var left = new NodeInfo()
+                    {
+                        Node = element.Node.LeftChild,
+                        Depth = depth,
+                    };
+                    bufer.Enqueue(left);
+                }
+                if (element.Node.RightChild != null)
+                {
+                    var right = new NodeInfo()
+                    {
+                        Node = element.Node.RightChild,
+                        Depth = depth,
+                    };
+                    bufer.Enqueue(right);
+                }
+            }
+
+            return returnArray.ToArray();
+        }
+    }
+
+    public class NodeInfo
+    {
+        public int Depth { get; set; }
+        public TreeNode Node { get; set; }
+    }
+
+}
--- task1/Program.cs
+++ task1/Program.cs
@@ -17,10 +17,24 @@ namespace task2
             tree.Root.Right.Left.Right = new Node(13, new Node(22), new Node(99));
             tree.Root.Right.Left.Right.Right = new Node(243, new Node(223), new Node(11));
 
+            Tree gbtree = new Tree(50);
+            gbtree.Root.LeftChild = new TreeNode(22);
+            gbtree.Root.RightChild = new TreeNode(25);
+            gbtree.Root.LeftChild.LeftChild = new TreeNode(28, new TreeNode(64), new TreeNode(34));
+            gbtree.Root.LeftChild.RightChild = new TreeNode(11);
+            gbtree.Root.RightChild.LeftChild = new TreeNode(5, new TreeNode(67), new TreeNode(46));
+            gbtree.Root.RightChild.RightChild = new TreeNode(77);
+            gbtree.Root.RightChild.LeftChild.LeftChild = new TreeNode(23, new TreeNode(51), new TreeNode(332));
+            gbtree.Root.RightChild.LeftChild.RightChild = new TreeNode(13, new TreeNode(22), new TreeNode(99));
+            gbtree.Root.RightChild.LeftChild.RightChild.RightChild = new TreeNode(243, new TreeNode(223), new TreeNode(11));
+
+
             Console.WriteLine("Бинарное дерево:");
             Console.WriteLine(new string('=', 40));            
             TreePrinter.Print(tree.Root);
             Console.WriteLine(new string('=', 40));
+
+            gbtree.PrintTree();
             
 
             Console.ReadLine();
